90faa0fccadc8b9436a609a2e041b1c9528e848c
parkavi-paragon <parkavitvm@gmail.com>
2014-10-20 15:49:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testparkavi::Application.config.secret_token = '6b30f0af237041fe44087fa3a64e3de0a22e19180dbd0544ec19eb1ac2587b751da692bea60f18a237120eaa140a7e8ca4e38416940c9940f19d44a410fe22a4'
